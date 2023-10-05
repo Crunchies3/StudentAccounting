687bcd46c2775072677f4f00da8656f0465f5b51
Crunchies3 <rivals191@gmail.com>
2023-10-05 13:44:10 +0800

    removed comment

--- prj_StudentAccounting/frm_loginStudent.vb
+++ prj_StudentAccounting/frm_loginStudent.vb
@@ -2,7 +2,5 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         frm_StudentDashboard.Show()
         Me.Hide()
-
-        'test'
     End Sub
 End Class
