e2c80af92fc41d783d389bd599406dd5ca14ba74
Crunchies3 <rivals191@gmail.com>
2023-10-05 20:57:27 +0800

    added comments in prcDisplaySubjects

--- prj_StudentAccounting/frm_sSubjects.vb
+++ prj_StudentAccounting/frm_sSubjects.vb
@@ -1,3 +1,13 @@
-﻿Public Class frm_sSubjects
+﻿Imports MySql.Data.MySqlClient
+Public Class frm_sSubjects
+    Private Sub frm_sSubjects_Load(sender As Object, e As EventArgs) Handles Me.Load
+        checkDatabaseConnection()
+    End Sub
+
+    Private Sub prcDisplaySubject()
+
+        'Diria ibutang ang code para i display ang subject na gi enroll sa isa ka student
+
+    End Sub
 
 End Class
