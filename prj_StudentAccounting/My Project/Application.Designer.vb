f40d8112723e62cfde927eb5e25857aa4acee211
Crunchies3 <rivals191@gmail.com>
2023-10-06 23:05:46 +0800

    added admin dashboard. (only design)

--- prj_StudentAccounting/My Project/Application.Designer.vb
+++ prj_StudentAccounting/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.prj_StudentAccounting.frm_loginStudent
+            Me.MainForm = Global.prj_StudentAccounting.frm_aDashBoard
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
