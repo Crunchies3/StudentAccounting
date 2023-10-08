37cfac844c8564579af497a45032b93ce5fffd72
Crunchies3 <rivals191@gmail.com>
2023-10-08 21:59:50 +0800

    added admin dashboard

--- prj_StudentAccounting/My Project/Application.Designer.vb
+++ prj_StudentAccounting/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.prj_StudentAccounting.frm_aDashBoard
+            Me.MainForm = Global.prj_StudentAccounting.frm_AdminDashboard
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
--- prj_StudentAccounting/frm_aDashBoard.vb
+++ prj_StudentAccounting/frm_aDashBoard.vb
@@ -1,21 +1,3 @@
 ﻿Public Class frm_aDashBoard
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
-    End Sub
-
-    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
-
-    End Sub
-
-    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
-
-    End Sub
-
-    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
-
-    End Sub
-
-    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
-
-    End Sub
 End Class
