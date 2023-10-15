7220e96090bbc5d540449eaf710dc9f546539e96
Crunchies3 <rivals191@gmail.com>
2023-10-15 13:03:07 +0800

    Finished design for aDashboard

--- prj_StudentAccounting/frm_aDashboard_v2.vb
+++ prj_StudentAccounting/frm_aDashboard_v2.vb
@@ -1,5 +1,11 @@
 ﻿Public Class frm_aDashboard_v2
-    Private Sub Label21_Click(sender As Object, e As EventArgs)
-
+    Dim access As Boolean = False
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TabControl1.SelectedIndex = 1
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TabControl1.SelectedIndex = 0
+    End Sub
+
 End Class
