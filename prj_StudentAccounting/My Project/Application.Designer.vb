db530d7855d3c56f0f2cfce04b6a643366397457
Crunchies3 <rivals191@gmail.com>
2023-10-16 12:41:17 +0800

    change login design

--- prj_StudentAccounting/My Project/Application.Designer.vb
+++ prj_StudentAccounting/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.prj_StudentAccounting.frm_AdminDashboard
+            Me.MainForm = Global.prj_StudentAccounting.frm_loginStudent
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
