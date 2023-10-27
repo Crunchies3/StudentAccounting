1fd4216d6d08908e650a42983a3bcf1125d6598f
Crunchies3 <rivals191@gmail.com>
2023-10-27 20:53:49 +0800

    Added necessary forms and function

---
+++ prj_StudentAccounting/frm_aSubjects.vb
@@ -0,0 +1,25 @@
+﻿Public Class frm_aSubjects
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TabControl1.SelectedIndex = 1
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        TabControl1.SelectedIndex = 2
+    End Sub
+
+    Private Sub btnAddStudent_Click(sender As Object, e As EventArgs) Handles btnAddStudent.Click
+        TabControl1.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TabControl1.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TabControl1.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TabControl1.SelectedIndex = 0
+    End Sub
+End Class
