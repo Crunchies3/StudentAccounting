9c4ef0163dc3f5af4903d19c837c86a0bbb535e3
kixks <kenmentzy@gmail.com>
2023-10-29 15:26:02 +0800

    Na change na nako cy. tung issue sa sDashboard ug sAssessment. sa sDashboard sa sql lang nako gibag.o

--- prj_StudentAccounting/frm_sAssessment.vb
+++ prj_StudentAccounting/frm_sAssessment.vb
@@ -18,55 +18,68 @@ Public Class frm_sAssessment
         dataTable = New DataTable
 
         With command
-
-            Dim totalUnits As Integer = 0
-            Dim totalAss As Double = 0
-            Dim perUnitFee As Double = 0
-
             .Parameters.Clear()
-            .CommandText = "prcGetTotalUnits"
+            .CommandText = "prcStudentFinalSubjectStatus"
             .CommandType = CommandType.StoredProcedure
-            .Parameters.AddWithValue("@p_id", userID)
             sqlDBAdapter.SelectCommand = command
             dataTable.Clear()
             sqlDBAdapter.Fill(dataTable)
 
-            totalUnits = dataTable.Rows(0).Item("totalunits")
+            If dataTable.Rows(0).Item("subjStatus") = True Then
+                With command
+
+                    Dim totalUnits As Integer = 0
+                    Dim totalAss As Double = 0
+                    Dim perUnitFee As Double = 0
+
+                    .Parameters.Clear()
+                    .CommandText = "prcGetTotalUnits"
+                    .CommandType = CommandType.StoredProcedure
+                    .Parameters.AddWithValue("@p_id", userID)
+                    sqlDBAdapter.SelectCommand = command
+                    dataTable.Clear()
+                    sqlDBAdapter.Fill(dataTable)
+
+                    totalUnits = dataTable.Rows(0).Item("totalunits")
 
 
-            .Parameters.Clear()
-            .CommandText = "prcDisplayStudentAssessment"
-            .CommandType = CommandType.StoredProcedure
-            .Parameters.AddWithValue("@p_id", studID)
-            sqlDBAdapter.SelectCommand = command
-            sqlDBAdapter.Fill(dataTable)
+                    .Parameters.Clear()
+                    .CommandText = "prcDisplayStudentAssessment"
+                    .CommandType = CommandType.StoredProcedure
+                    .Parameters.AddWithValue("@p_id", studID)
+                    sqlDBAdapter.SelectCommand = command
+                    sqlDBAdapter.Fill(dataTable)
 
-            perUnitFee = dataTable.Rows(1).Item("amount")
+                    perUnitFee = dataTable.Rows(1).Item("amount")
 
-            dataTable.Rows(1)("amount") = totalUnits * (dataTable.Rows(1).Item("amount"))
+                    dataTable.Rows(1)("amount") = totalUnits * (dataTable.Rows(1).Item("amount"))
 
-            If dataTable.Rows.Count > 0 Then
-                dgv_assessment.RowCount = dataTable.Rows.Count - 1
-                row = 1
+                    If dataTable.Rows.Count > 0 Then
+                        dgv_assessment.RowCount = dataTable.Rows.Count - 1
+                        row = 1
 
-                While Not dataTable.Rows.Count - 1 < row
+                        While Not dataTable.Rows.Count - 1 < row
 
-                    If row = 1 Then
-                        totalAss = totalAss + dataTable.Rows(row).Item("amount")
-                        dgv_assessment.Rows(row - 1).Cells(0).Value = dataTable.Rows(row).Item("description") & " (" & perUnitFee & " X " & totalUnits & ")".ToString
-                        dgv_assessment.Rows(row - 1).Cells(1).Value = "₱ " & dataTable.Rows(row).Item("amount").ToString
-                        row = row + 1
-                    Else
-                        totalAss = totalAss + dataTable.Rows(row).Item("amount")
-                        dgv_assessment.Rows(row - 1).Cells(0).Value = dataTable.Rows(row).Item("description").ToString
-                        dgv_assessment.Rows(row - 1).Cells(1).Value = "₱ " & dataTable.Rows(row).Item("amount").ToString
-                        row = row + 1
+                            If row = 1 Then
+                                totalAss = totalAss + dataTable.Rows(row).Item("amount")
+                                dgv_assessment.Rows(row - 1).Cells(0).Value = dataTable.Rows(row).Item("description") & " (" & perUnitFee & " X " & totalUnits & ")".ToString
+                                dgv_assessment.Rows(row - 1).Cells(1).Value = "₱ " & dataTable.Rows(row).Item("amount").ToString
+                                row = row + 1
+                            Else
+                                totalAss = totalAss + dataTable.Rows(row).Item("amount")
+                                dgv_assessment.Rows(row - 1).Cells(0).Value = dataTable.Rows(row).Item("description").ToString
+                                dgv_assessment.Rows(row - 1).Cells(1).Value = "₱ " & dataTable.Rows(row).Item("amount").ToString
+                                row = row + 1
+                            End If
+
+                        End While
                     End If
 
-                End While
+                    lbl_totalAssessment.Text = "₱ " & totalAss
+                End With
+            Else
+                MessageBox.Show("Atleast submit 1 finalsubject to display shenanigans", "Submit a Final Subject", MessageBoxButtons.OK, MessageBoxIcon.Information)
             End If
-
-            lbl_totalAssessment.Text = "₱ " & totalAss
 
         End With
 
