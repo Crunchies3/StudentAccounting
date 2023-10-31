c97d2b50e8e239609bff5ac7e2a0ae670caf5365
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2023-10-31 18:18:47 +0800

    remove an error regarding pictures

--- prj_StudentAccounting/My Project/Resources.Designer.vb
+++ prj_StudentAccounting/My Project/Resources.Designer.vb
@@ -63,16 +63,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property _1() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("1", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property _666007069677() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("666007069677", resourceCulture)
