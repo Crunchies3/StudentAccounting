e9d70806ee1502e36ac3cd24bad848259cda25ec
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2023-12-28 01:32:42 +0800

    maka view ug payment sa student page, g hawa ang duplicate na enrolled students

--- prj_StudentAccounting/ModGlobalProcedure.vb
+++ prj_StudentAccounting/ModGlobalProcedure.vb
@@ -8,7 +8,7 @@ Module ModGlobalProcedure
             server_name = "localhost"
             DB_name = "db_studentaccountingmanagement"
             DB_user = "root"
-            DB_pass = "root"
+            DB_pass = "cyril2003"
             port = "3306"
 
 
